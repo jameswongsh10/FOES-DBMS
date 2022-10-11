fb59fc7e3b59746e6bffc2822eee50418667d542
jameswongsh10 <62973874+jameswongsh10@users.noreply.github.com>
2022-10-11 12:46:53 +0800

    Added error handling for downloading attachment

--- app/Http/Controllers/AttachmentStaffController.php
+++ app/Http/Controllers/AttachmentStaffController.php
@@ -225,7 +225,7 @@ class AttachmentStaffController extends Controller
 
     public function downloadAttachment($id)
     {
-//        if (Auth::check()) {
+        if (Auth::check()) {
             try {
                 $attachment = AttachmentStaff::find($id);
                 if (is_null($attachment)) {
@@ -247,10 +247,10 @@ class AttachmentStaffController extends Controller
                     'message' => $e->errorInfo[2]
                 ], 400);
             }
-//        }
-//        return response()->json([
-//            'status' => false,
-//            'message' => 'Unauthorized user'
-//        ], 401);
+        }
+        return response()->json([
+            'status' => false,
+            'message' => 'Unauthorized user'
+        ], 401);
     }
 }
